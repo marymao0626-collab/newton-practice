a5f30259921d2d4401b9a801cf019533a7f4eba5
Mary Mao <marymao26@berkeley.edu>
2026-08-19 14:25:22 -0700

    Add documentation to Newton module

--- newton.py
+++ newton.py
@@ -1,22 +1,25 @@
 def first_derivative(f, x, h=1e-5):
+    """Approximate the first derivative of f at x."""
     return (f(x + h) - f(x)) / h
 
 def second_derivative(f, x, h=1e-5):
-    return derivative (lambda y : deriative(f, y, h), x, h)
+    """Approximate the second derivative of f at x."""
+    return first_derivative (lambda y : first_derivative(f, y, h), x, h)
 
 def optimize(f, x0, tol=1e-6):
+    """Find a local minimum of f using Newton's method."""
     x = x0
 
     while True:
         first = first_derivative(f, x)
         second = second_derivative(f, x)
 
-        x_1 = x - first / second
+        x_new = x - first / second
 
         if abs(x_new - x) < tol:
             return x_new
 
-        x = x_1
+        x = x_new
         
 
 
